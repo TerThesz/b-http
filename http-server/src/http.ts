8e34cebcb47f0115da6dcd3a12bd6c8dcc596529
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-07-22 17:56:47 +0200

    tcp

--- http-server/src/http.ts
+++ http-server/src/http.ts
@@ -1,1 +1,7 @@
-console.log('Hello World');
+import net from 'net';
+import { Socket } from 'net';
+
+const server = net.createServer((socket: Socket) => {
+});
+
+server.listen(3000, '0.0.0.0', () => console.log('🏃 on port 3000'));
