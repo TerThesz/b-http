d0fdb4a5f67663f259d8095f03dcc95e425648a0
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-07-22 18:10:57 +0200

    simple response

--- http-server/src/http.ts
+++ http-server/src/http.ts
@@ -2,6 +2,11 @@ import net from 'net';
 import { Socket } from 'net';
 
 const server = net.createServer((socket: Socket) => {
+  console.log(socket.remoteAddress);
+  socket.on('data', buf => {
+    socket.write('HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n{"hello": "world"}');
+    socket.end();
+  });
 });
 
 server.listen(3000, '0.0.0.0', () => console.log('🏃 on port 3000'));
