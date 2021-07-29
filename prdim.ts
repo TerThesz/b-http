31777c364e5ec2e6bd25810850902c3baa554593
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-07-29 23:41:48 +0200

    Delete prdim.ts

--- prdim.ts
+++
@@ -1,4 +0,0 @@
-import { createServer, Router } from './src/http';
-const router = new Router();
-
-createServer(router.get('/', (req: any, res: any) => res.json({dano: "smrdi", mato: "vonia"}))).listen(6969);
