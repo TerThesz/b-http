a8ae4f44679eecfd07a2481e1202f4035acad7dc
TerThesz <60745997+TerThesz@users.noreply.github.com>
2021-07-31 23:44:40 +0200

    use

--- src/http_server.ts
+++ src/http_server.ts
@@ -6,17 +6,13 @@ import { InitEvents } from './http_events';
 function Server(this: any, routers: any[]) {
   if (!(this instanceof Server)) return new (Server as any)(this, routers);
 
-  /* if (!routers?.length) throw new Error('Define RequestListener'); */
+  if (!routers?.length) throw new Error('Define RequestListener');
 
-  (this as This).routers = routers;
-
-  (this as This)._netServer = net.createServer((socket: Socket) => new (InitEvents as any)(socket).data(this.routers));
+  (this as This)._netServer = net.createServer((socket: Socket) => new (InitEvents as any)(socket).data(routers));
 
   return this;
 }
 
 Server.prototype.listen = function listen(this: any, ...args: Array<string | number | Function>) { this._netServer.listen(...args) }
-
-Server.prototype.use = function use(this: any, router: any) { this.routers.push(router) }
 
 export { Server };
